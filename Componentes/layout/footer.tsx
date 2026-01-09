93f537f5a12c73849a4a4020fda468fdc472b8a1
Héctor Manuel Lomelin <lomelinhectorm@gmail.com>
2026-01-09 03:34:14 -0600

    Add Footer component with copyright notice

---
+++ Componentes/layout/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="border-t border-neutral-900 py-12 text-center text-sm text-gray-500">
+      © {new Date().getFullYear()} H Manuel Lomelín.  
+      Pensar también es decidir.
+    </footer>
+  );
+}
